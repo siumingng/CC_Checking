3c2ec65244daf0492ba9dbdf0bcbabd6da4c7fdb
siumingng <siuming@bpohk.com>
2018-07-11 14:07:20 +0800

    Modified the Summary Page
    Modified CC_Summary.php

--- CC_Summary.php
+++ CC_Summary.php
@@ -9,7 +9,7 @@
 <?php
 	session_start();
 	$serverName = "172.23.4.250"; //serverName\instanceName
-	$connectionInfo = array( "Database"=>"FLP", "UID"=>"sa", "PWD"=>"p@ssw0rd", "CharacterSet" => "UTF-8");
+	$connectionInfo = array( "Database"=>"FLPNDP", "UID"=>"sa", "PWD"=>"p@ssw0rd", "CharacterSet" => "UTF-8");
 	$conn = sqlsrv_connect( $serverName, $connectionInfo);
 	if( isset($_POST['fboid']) )
 	{
@@ -20,9 +20,13 @@
 	$fboname="";
 	$_SESSION["dist"]=$dist;
 	
-	$pcc=0;
-	$nmcc=0;
-	$totalcc=0;
+	$lmpcc=0;
+	$lmnmcc=0;
+	$lmtotalcc=0;
+	
+	$cmpcc=0;
+	$cmnmcc=0;
+	$cmtotalcc=0;
 	
 	if( $conn ) {
 		$sql_getname = "Select * from tblappliciant where dist_id='" . $dist . "'";
@@ -31,31 +35,80 @@
 		{
 			$fboname=$row['name'];
 		}
-		$sql_callsp = "Exec dbo.getDownLineOrder '" . $dist ."'";
-		$stmt3 = sqlsrv_query( $conn, $sql_callsp);
-		if( $stmt3 === false )
+		
+		//Last Month PCC
+		$sql_get_lm_pcc = "select SUM(total_cc) as pcc from GetLastMonthPOrder(?)";
+		$params=array($dist);
+		$result = sqlsrv_query( $conn, $sql_get_lm_pcc,$params);
+		if ($result === false )
 		{
-			echo "Error in executing statement 3.\n";
 			die( print_r( sqlsrv_errors(), true));
-		}
-		while( $row = sqlsrv_fetch_array( $stmt3, SQLSRV_FETCH_ASSOC) ) 
+		}else
 		{
-			if($row['dist_id'] == $dist)
-			{
-				$pcc=$pcc+$row['total_cc'];			
-				
-			} else
-			{
-				$nmcc=$nmcc + $row['total_cc'];
-			}
-			$totalcc =$totalcc + $row['total_cc'];
-      		
+			 $obj = sqlsrv_fetch_object( $result );
+			 $lmpcc= $obj->pcc;			
+			
 		}
-		echo "箱積分（CC） - 5月份 </BR>";
+		
+		//Last Month NMCC
+		$sql_get_lm_nmcc = "select SUM(total_cc) as nmcc from GetLastMonthNMOrder(?)";
+		$params=array($dist);
+		$result = sqlsrv_query( $conn, $sql_get_lm_nmcc,$params);
+		if ($result === false )
+		{
+			die( print_r( sqlsrv_errors(), true));
+		}else
+		{
+			 $obj = sqlsrv_fetch_object( $result );
+			 $lmnmcc= $obj->nmcc;			
+			
+		}
+		
+		//Last Month Total CC
+		$lmtotalcc = $lmpcc + $lmnmcc;
+		
+		//Current Month PCC
+		$sql_get_cm_pcc = "select SUM(total_cc) as pcc from GetCurrentMonthPOrder(?)";
+		$params=array($dist);
+		$result = sqlsrv_query( $conn, $sql_get_cm_pcc,$params);
+		if ($result === false )
+		{
+			die( print_r( sqlsrv_errors(), true));
+		}else
+		{
+			 $obj = sqlsrv_fetch_object( $result );
+			 $cmpcc= $obj->pcc;			
+			
+		}
+		
+		//Current Month NMCC
+		$sql_get_cm_nmcc = "select SUM(total_cc) as nmcc from GetCurrentMonthNMOrder(?)";
+		$params=array($dist);
+		$result = sqlsrv_query( $conn, $sql_get_cm_nmcc,$params);
+		if ($result === false )
+		{
+			die( print_r( sqlsrv_errors(), true));
+		}else
+		{
+			 $obj = sqlsrv_fetch_object( $result );
+			 $cmnmcc= $obj->nmcc;			
+			
+		}
+		
+		//Current Month Total CC
+		$cmtotalcc = $cmpcc + $cmnmcc;
+		
+		
+		echo "箱積分（CC）</BR>";
 		echo "FBO: " . $fboname . " (". $dist . " )" ."</BR></BR>";
-		echo "Personal CC: <a href='CC_PDetails.php'>". $pcc . "</a></BR>";
-		echo "Non-Manager CC:  <a href='CC_NMDetails.php'> ". $nmcc . "</a></BR>";
-		echo "Total CC: ". $totalcc . "</BR>";
+		echo "Last Month Personal CC: <a href='CC_PDetails.php'>". $lmpcc . "</a></BR>";
+		echo "Last Month Non-Manager CC: <a href='CC_PDetails.php'>". $lmnmcc . "</a></BR>";
+		echo "Last Month Total CC: <a href='CC_PDetails.php'>". $lmtotalcc . "</a></BR>";
+		echo "</BR>";
+		echo "Current Month Personal CC: <a href='CC_PDetails.php'>". $cmpcc . "</a></BR>";
+		echo "Current Month Non-Manager CC: <a href='CC_PDetails.php'>". $cmnmcc . "</a></BR>";
+		echo "Current Month Total CC: <a href='CC_PDetails.php'>". $cmtotalcc . "</a></BR>";
+		
 		
 	}else{
 		 echo "Connection could not be established.<br />";
